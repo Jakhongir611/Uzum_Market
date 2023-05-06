ad32c5b5acbe3cca9fe208a489e17a6ad2e5a18f
Jahongir611 <khurramovjahongir611@gmail.com>
2023-05-06 15:14:09 +0500

    go

--- js/script.js
+++ js/script.js
@@ -5,15 +5,34 @@ let catalog = doc.querySelector('.catalog');
 let language = doc.querySelector('.language');
 let flag = doc.querySelector('#flag');
 let btn_more = doc.querySelector('.btn-more');
+let location_span = doc.querySelector('.location_span');
+let my_city_modal = doc.querySelector('.my_city_modal');
+let my_city_x = doc.querySelector('.x');
+let city_title = doc.querySelectorAll('.city_title');
+let sliders = doc.querySelector('.sliders')
 
 
 
-language.addEventListener('click', () => {
-  
+location_span.addEventListener('click', () => {
+  my_city_modal.classList.remove('none');
+  body.style.overflow = 'hidden'
+})
+my_city_x.addEventListener('click', () => {
+  my_city_modal.classList.add('none');
+  body.style.overflow = 'auto'
+})
+
+city_title.forEach(item => {
+  item.addEventListener('click', () => {
+    location_span.textContent = item.textContent;
+    city_title.style.color = '#6E00FF';
+  })
 })
 
 
-
+sliders.addEventListener('click', () =>{
+  sliders.style.cursor = "grabbing"
+})
 
 
 
